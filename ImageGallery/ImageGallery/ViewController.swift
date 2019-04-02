2a755d891ca55c2fa959e5dbc772ea98ef7c2e91
Daniel Bermúdez <daniel.bermudez@endava.com>
2019-04-02 10:10:01 -0500

    Initial Proyect

---
+++ ImageGallery/ImageGallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageGallery
+//
+//  Created by Daniel Bermudez on 4/2/19.
+//  Copyright © 2019 Endava. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
